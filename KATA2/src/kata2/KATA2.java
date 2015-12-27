c476135d9270d4fb57890145c9613f11aef835b5
tricornio <pablo_isleta@hotmail.com>
2015-12-27 20:40:46 +0000

    practica 2 histograma

--- KATA2/src/kata2/KATA2.java
+++ KATA2/src/kata2/KATA2.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata2;
 
-/**
- *
- * @author Admin
- */
-public class KATA2 {
+import java.util.HashMap;
 
-    /**
-     * @param args the command line arguments
-     */
+public class KATA2 {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Integer[] array = {2,2,11,4,2,6,11,2,3,5,11,5,100};
+        String[] array1 = {"Ana", "Pablo", "Pedro", "Pablo", "Pedro", "Pablo", "Pedro"};    
+        
+        Histogram <Integer> histogram = BuildHisto.computHisto(array);
+        
+        for (Object key : histogram.keySet()) {
+            System.out.println(key + ":" + histogram.get(key));
+        }
+        
     }
     
 }
