1fcf42f9c0991e159cd84601dda71495563a4c15
tricornio <pablo_isleta@hotmail.com>
2016-01-02 03:12:57 +0000

    dominios kata 3

--- kata3/src/kata3/KATA3.java
+++ kata3/src/kata3/KATA3.java
@@ -1,5 +1,8 @@
 package kata3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class KATA3 {
 
     /**
@@ -7,28 +10,25 @@ public class KATA3 {
      */
     public static void main(String[] args) {
         
-        
-        
         Histogram<String> histo = new Histogram<>();
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
+        String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\kata3\\Data\\emailFile.txt"; //ruta del fichero
         
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        histo.increment("ulpgc.es");
-        
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
-        histo.increment("hotmail.com");
+        //tratamiento del fichero
+        try {
+            
+            BufferedReader fileIn = new BufferedReader( new FileReader(pathName) );
+            
+            String mail;
+            
+            while( (mail = fileIn.readLine()) != null ){
+                if( ! mail.contains("@") )
+                    continue;
+            }
+            histo.increment( mail.split("@")[1].toLowerCase() );
+            
+        } catch (Exception e) {
+            System.out.println("El fichero no existe");
+        }
         
         HistogramDisplay histoDisplay = new HistogramDisplay(histo);
         histoDisplay.execute();
