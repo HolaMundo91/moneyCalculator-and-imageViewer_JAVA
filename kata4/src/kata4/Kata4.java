a60d5acd88f9306cde33469c9294a8493b9af54d
tricornio <pablo_isleta@hotmail.com>
2016-01-03 02:12:51 +0000

    copiar imagenes kata4

--- kata4/src/kata4/Kata4.java
+++ kata4/src/kata4/Kata4.java
@@ -1,10 +1,16 @@
 package kata4;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         String path = "C:\\Users\\usuario\\Documents";
         File file = new File(path);
@@ -15,9 +21,35 @@ public class Kata4 {
             // System.out.println(fileList1);
         }
         
-            print( file.listFiles(),"" );
+        //print( file.listFiles(), "" );
+ 
+        String from = "C:\\Users\\usuario\\Downloads\\Chrysanthemum.jpg";
+        String to = "C:\\Users\\usuario\\Downloads\\Chrysanthemum(1).jpg";
         
-    }
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        
+        byte[] buffer = new byte [1024];
+        
+        Date date = new Date();
+        
+        while(true){
+            
+            int read = input.read(buffer);
+            
+            if (read < 0){
+                break;
+            }
+            output.write(buffer);
+            
+        }
+        System.out.println( new Date().getTime()-date.getTime()+" msg" );
+        
+        input.close();
+        output.flush();
+        output.close();
+        
+    }   
 
     private static void print(File[] listFiles, String ident) {
 
